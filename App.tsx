231f6f2294c7268f87ca9687e00c9224c012c9de
Your Name <you@example.com>
2023-04-16 22:30:01 -0400

    feat: finish project 1 react native

--- App.tsx
+++ App.tsx
@@ -6,8 +6,8 @@ export default function App() {
     <>
       <StatusBar
         style="light"
-      // backgroundColor="transparent"
-      // translucent
+        backgroundColor="transparent"
+        translucent
       />
       <Home />
     </>
